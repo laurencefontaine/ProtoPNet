20f4112c04d87c9514e5339415b32b32a3fad5a1
leo.guerin-morneau <leo.guerin-morneau@celliersintelligents.com>
2023-05-04 12:58:56 -0400

    petit fix

--- data_prep.py
+++ data_prep.py
@@ -21,7 +21,7 @@ def main():
             os.makedirs(os.path.dirname(dest_fpath), exist_ok=True)
             shutil.copy("CUB_200_2011/CUB_200_2011/images/" + path, dest_fpath)
 
-            dest_fpath = "./datasets/cub200_whole/test_cropped/" + path
+            dest_fpath = "./datasets/cub200_cropped/test_cropped/" + path
             os.makedirs(os.path.dirname(dest_fpath), exist_ok=True)
             im.save(dest_fpath)
         else:
@@ -29,7 +29,7 @@ def main():
             os.makedirs(os.path.dirname(dest_fpath), exist_ok=True)
             shutil.copy("CUB_200_2011/CUB_200_2011/images/" + path, dest_fpath)
 
-            dest_fpath = "./datasets/cub200_whole/train_cropped/" + path
+            dest_fpath = "./datasets/cub200_cropped/train_cropped/" + path
             os.makedirs(os.path.dirname(dest_fpath), exist_ok=True)
             im.save(dest_fpath)
 
